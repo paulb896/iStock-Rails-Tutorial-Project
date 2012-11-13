b13f8c8fb1b0582a70c3e32f494f3c28d3bef57f
Anis Zarei <azarei@gettyimages.com>
2012-11-13 09:21:09 -0700

    Added base rails app with cucumber & rpsec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IStockRailsTutorialProject::Application.config.secret_token = '26f8a255a7d010fca873698293ec9800f560c4c340ddee7abc82804626be772e292aa95755bda5883b539421a632bdc47cd4191ce8c2e768cd4800db6e1175d8'
